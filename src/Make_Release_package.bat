207a2fa16368e79d65e71ffa0308bccda36a88b3
Matthias-Beckmann-Cyncly <matthias.beckmann@cyncly.com>
2024-09-09 16:28:31 +0200

    Move everything from src folder up to root folder

--- src/Make_Release_package.bat
+++
@@ -1,8 +0,0 @@
-SET BASEDIR=%~dp0..\
-SET RELEASEDIR=%BASEDIR%release
-SET TARGETZIP="%RELEASEDIR%\CopyProdToTest.zip"
-DEL %RELEASEDIR%\CopyProdToTest.zip
-
-"C:\Program Files\WinRAR\WinRAR.exe" a %RELEASEDIR%\CopyProdToTest.zip Template Tools Call_CopyProdToTest.bat CHANGELOG.txt IDEAS.txt Insight_CopyProdToTest_InstallationGuide.pdf LICENSE.txt
-pause
-
