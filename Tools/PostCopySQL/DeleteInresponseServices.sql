dbc2fec2a799a7b5a0dff8e836cac83a199415d6
Matthias-Beckmann-Cyncly <matthias.beckmann@cyncly.com>
2025-02-25 11:07:28 +0100

    Delete also Inresponse Services of type "Web Service"

--- Tools/PostCopySQL/DeleteInresponseServices.sql
+++ Tools/PostCopySQL/DeleteInresponseServices.sql
@@ -1,7 +1,7 @@
--- Delete Services of Type 1=Standard and 5=Backend and their child services:
+-- Delete Services of Type 1=Standard, 2=Web Services and 5=Backend and their child services:
 
 IF OBJECT_ID(N'inresponse.ServiceServiceNotifications', N'U') IS NOT NULL
-	DELETE FROM inResponse.ServiceServiceNotifications WHERE svcID IN (SELECT svcID FROM inresponse.Services WHERE istID IN (1,5))
-DELETE FROM inresponse.Services WHERE svcIDParent IN (SELECT svcID FROM inresponse.Services WHERE istID IN (1,5))
-DELETE FROM inresponse.Services WHERE istID IN (1,5)
+	DELETE FROM inResponse.ServiceServiceNotifications WHERE svcID IN (SELECT svcID FROM inresponse.Services WHERE istID IN (1,2,5))
+DELETE FROM inresponse.Services WHERE svcIDParent IN (SELECT svcID FROM inresponse.Services WHERE istID IN (1,2,5))
+DELETE FROM inresponse.Services WHERE istID IN (1,2,5)
 
